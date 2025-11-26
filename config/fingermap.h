898366bd298d900eeea66a880dff0766fe9cbb25
newx - Isaac Turner <156273295+turnisaa-sgtworms@users.noreply.github.com>
2025-11-26 09:57:37 -0700

    Rename key definitions for layers and shortcuts

--- config/fingermap.h
+++ config/fingermap.h
@@ -77,113 +77,171 @@
 // layer 0 - default
 	// left hand
 		// top row
-			#define 0t0 	&kp TAB
-			#define 0t1 	&kp V
-			#define 0t2 	&kp Y
-			#define 0t3 	&oamt 0 O
-			#define 0t4 	&uemt 0 U
-			#define 0t5 	&zemt Z 0
+			#define _0t0 	&kp TAB
+			#define _0t1 	&kp V
+			#define _0t2 	&kp Y
+			#define _0t3 	&oamt 0 O
+			#define _0t4 	&uemt 0 U
+			#define _0t5 	&zemt Z 0
 		// home row
-			#define 0h0 	&amt LC(DEL) DEL
-			#define 0h1 	&hml LSHIFT C
-			#define 0h2 	&hml LALT I
-			#define 0h3 	&hml LGUI A
-			#define 0h4 	&hml LCTRL E
-			#define 0h5 	&qumt Q 0
+			#define _0h0 	&amt LC(DEL) DEL
+			#define _0h1 	&hml LSHIFT C
+			#define _0h2 	&hml LALT I
+			#define _0h3 	&hml LGUI A
+			#define _0h4 	&hml LCTRL E
+			#define _0h5 	&qumt Q 0
 		// bottom row
-			#define 0b0 	&amt LC(BSPC) BSPC
-			#define 0b1 	&kp LSHIFT
-			#define 0b2 	&mage_j
-			#define 0b3 	&kp X
-			#define 0b4 	&mage_rep
-			#define 0b5 	&none
+			#define _0b0 	&amt LC(BSPC) BSPC
+			#define _0b1 	&kp LSHIFT
+			#define _0b2 	&mage_j
+			#define _0b3 	&kp X
+			#define _0b4 	&mage_rep
+			#define _0b5 	&none
 	
 	// right hand
 		// top row
-			#define 0t6 	&kp P
-			#define 0t7 	&kp D
-			#define 0t8 	&kp L
-			#define 0t9 	&kp F
-			#define 0t10 	&kp B
-			#define 0t11 	&kp ESC
+			#define _0t6 	&kp P
+			#define _0t7 	&kp D
+			#define _0t8 	&kp L
+			#define _0t9 	&kp F
+			#define _0t10 	&kp B
+			#define _0t11 	&kp ESC
 		// home row
-			#define 0h6 	&kp W
-			#define 0h7 	&hmr RCTRL T
-			#define 0h8 	&hmr RGUI H
-			#define 0h9 	&hmr RALT S
-			#define 0h10	&hmr RSHIFT N
-			#define 0h11	&amt RC(BSPC) BSPC
+			#define _0h6 	&kp W
+			#define _0h7 	&hmr RCTRL T
+			#define _0h8 	&hmr RGUI H
+			#define _0h9 	&hmr RALT S
+			#define _0h10	&hmr RSHIFT N
+			#define _0h11	&amt RC(BSPC) BSPC
 		// bottom row
-			#define 0b6 	&none
-			#define 0b7 	&mage_k
-			#define 0b8 	&kp M
-			#define 0b9 	&kp G
-			#define 0b10	&kp RSHIFT
-			#define 0b11	&amt RC(DEL) DEL
+			#define _0b6 	&none
+			#define _0b7 	&mage_k
+			#define _0b8 	&kp M
+			#define _0b9 	&kp G
+			#define _0b10	&kp RSHIFT
+			#define _0b11	&amt RC(DEL) DEL
 
 	// thumbfan
 		// 
-			#define 0thlN 	&none
-			#define 0thlH 	&lt 1 R
-			#define 0thlF	&none
+			#define _0thlN 	&none
+			#define _0thlH 	&lt 1 R
+			#define _0thlF	&none
 			
-			#define 0thrF 	&none
-			#define	0thrH	&lt 2 SPACE
-			#define 0thrN	&none
+			#define _0thrF 	&none
+			#define	_0thrH	&lt 2 SPACE
+			#define _0thrN	&none
 
 // layer 1 - symbols
 	// left hand
 		// top row
-			#define 1t0 	&kp TAB
-			#define 1t1 	&kp V
-			#define 1t2 	&kp Y
-			#define 1t3 	&oamt 0 O
-			#define 1t4 	&uemt 0 U
-			#define 1t5 	&zemt Z 0
+			#define _1t0 	&kp TAB
+			#define _1t1 	&none
+			#define _1t2 	&amt LS(N3) 	LS(N5)
+			#define _1t3 	&amt LS(N6) 	LS(N2)
+			#define _1t4 	&amt LS(EQUAL) 	MINUS
+			#define _1t5 	&amt PIPE 		GRAVE
 		// home row
-			#define 1h0 	&amt LC(DEL) DEL
-			#define 1h1 	&hml LSHIFT C
-			#define 1h2 	&hml LALT I
-			#define 1h3 	&hml LGUI A
-			#define 1h4 	&hml LCTRL E
-			#define 1h5 	&qumt Q 0
+			#define _1h0 	&amt LC(DEL) 	DEL
+			#define _1h1 	&amt LS(MINUS)	N3
+			#define _1h2 	&amt LS(N7) 	N2
+			#define _1h3 	&amt EQUAL 		N1
+			#define _1h4 	&amt SEMI 		N0
+			#define _1h5 	&amt LS(GRAVE) 	N4
 		// bottom row
-			#define 1b0 	&amt LC(BSPC) BSPC
-			#define 1b1 	&kp LSHIFT
-			#define 1b2 	&mage_j
-			#define 1b3 	&kp X
-			#define 1b4 	&mage_rep
-			#define 1b5 	&none
+			#define _1b0 	&amt LC(BSPC) 	BSPC
+			#define _1b1 	&kp LSHIFT
+			#define _1b2 	&amt LBKT 		LS(COMMA)
+			#define _1b3 	&amt LS(LBKT) 	DQT
+			#define _1b4 	&amt LPAR 		COMMA
+			#define _1b5 	&none
 	
 	// right hand
 		// top row
-			#define 1t6 	&kp P
-			#define 1t7 	&kp D
-			#define 1t8 	&kp L
-			#define 1t9 	&kp F
-			#define 1t10 	&kp B
-			#define 1t11 	&kp ESC
+			#define _1t6 	&amt BSLH 		FSLH
+			#define _1t7 	&parmt 0 		0 
+			#define _1t8 	&brcmt 0 		0
+			#define _1t9 	&bktmt 0 		0
+			#define _1t10 	&none
+			#define _1t11 	&kp ESC
 		// home row
-			#define 1h6 	&kp W
-			#define 1h7 	&hmr RCTRL T
-			#define 1h8 	&hmr RGUI H
-			#define 1h9 	&hmr RALT S
-			#define 1h10	&hmr RSHIFT N
-			#define 1h11	&amt RC(BSPC) BSPC
+			#define _1h6 	&amt LS(N4) 	N9
+			#define _1h7 	&amt COLON 		N5
+			#define _1h8 	&amt LS(N8) 	N6
+			#define _1h9 	&amt LS(N1) 	N7
+			#define _1h10	&amt LS(FSLH) 	N8
+			#define _1h11	&amt RC(BSPC) 	BSPC
 		// bottom row
-			#define 1b6 	&none
-			#define 1b7 	&mage_k
-			#define 1b8 	&kp M
-			#define 1b9 	&kp G
-			#define 1b10	&kp RSHIFT
-			#define 1b11	&amt RC(DEL) DEL
+			#define _1b6 	&none
+			#define _1b7 	&amt RPAR 		DOT
+			#define _1b8 	&amt LS(RBKT) 	APOS
+			#define _1b9 	&amt RBKT 		LS(DOT)
+			#define _1b10	&kp RSHIFT
+			#define _1b11	&amt RC(DEL) 	DEL
 
 	// thumbfan
 		// 
-			#define 1thlN 	&none
-			#define 1thlH 	&lt 1 R
-			#define 1thlF	&none
+			#define _1thlN 	&none
+			#define _1thlH 	&none
+			#define _1thlF	&none
 			
-			#define 1thrF 	&none
-			#define	1thrH	&lt 2 SPACE
-			#define 1thrN	&none
+			#define _1thrF 	&none
+			#define	_1thrH	&kp SPACE
+			#define _1thrN	&none
+
+// layer 2 - shortcuts
+	// left hand
+		// top row
+			#define _2t0 	&kp TAB
+			#define _2t1 	&none
+			#define _2t2 	&amt LC(LS(W)) 	LC(W)
+			#define _2t3 	&amt LC(LS(R)) 	LC(R)
+			#define _2t4 	&amt LC(LS(T)) 	LC(T)
+			#define _2t5 	&amt LC(LS(Y)) 	LC(Y)
+		// home row
+			#define _2h0 	&amt LC(DEL) 	DEL
+			#define _2h1 	&amt LC(LS(A)) 	LC(A)
+			#define _2h2 	&amt LC(LS(X)) 	LC(X)
+			#define _2h3 	&amt LC(LS(C)) 	LC(C)
+			#define _2h4 	&amt LC(LS(V)) 	LC(V)
+			#define _2h5 	&amt LC(LS(Z)) 	LC(Z)
+		// bottom row
+			#define _2b0 	&amt LC(BSPC) 	BSPC
+			#define _2b1 	&kp LSHIFT
+			#define _2b2 	&amt LC(LS(S)) 	LC(S)
+			#define _2b3 	&amt LC(LS(D)) 	LC(D)
+			#define _2b4 	&amt LC(LS(F)) 	LC(F)
+			#define _2b5 	&none
+	
+	// right hand
+		// top row
+			#define _2t6 	&kp C_PP
+			#define _2t7 	&kp C_PREV
+			#define _2t8 	&kp C_NEXT
+			#define _2t9 	&kp PGUP
+			#define _2t10 	&none
+			#define _2t11 	&kp ESC
+		// home row
+			#define _2h6 	&kp C_MUTE 		
+			#define _2h7 	&kp LEFT   		
+			#define _2h8 	&kp UP	 			
+			#define _2h9 	&kp DOWN 			
+			#define _2h10	&kp RIGHT
+			#define _2h11	&amt RC(BSPC) 	BSPC
+		// bottom row
+			#define _2b6 	&none
+			#define _2b7 	&kp HOME   		
+			#define _2b8 	&kp END 	 		
+			#define _2b9 	&kp PGDN
+			#define _2b10	&kp RSHIFT
+			#define _2b11	&amt RC(DEL) 	DEL
+
+	// thumbfan
+		// 
+			#define _2thlN 	&none
+			#define _2thlH 	&kp PSCRN
+			#define _2thlF	&none
+			
+			#define _2thrF 	&none
+			#define	_2thrH	&none
+			#define _2thrN	&none
+

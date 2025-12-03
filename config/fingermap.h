0aafe08011703773683e5f113dbd1e904d393e79
newx - Isaac Turner <156273295+turnisaa-sgtworms@users.noreply.github.com>
2025-12-03 16:38:43 -0700

    Update fingermap.h

--- config/fingermap.h
+++ config/fingermap.h
@@ -89,10 +89,10 @@
 			#define _0t5 	&zemt Z 0
 		// home row
 			#define _0h0 	&amt LC(DEL) DEL
-			#define _0h1 	&kp C
-			#define _0h2 	&kp I
-			#define _0h3 	&kp A
-			#define _0h4 	&kp E
+			#define _0h1 	&hml LALT C
+			#define _0h2 	&hml LCTRL I
+			#define _0h3 	&hml LGUI A
+			#define _0h4 	&hml LSHIFT E
 			#define _0h5 	&qumt Q 0
 		// bottom row
 			#define _0b0 	&amt LC(BSPC) BSPC
@@ -112,10 +112,10 @@
 			#define _0t11 	&kp ESC
 		// home row
 			#define _0h6 	&kp W
-			#define _0h7 	&kp T
-			#define _0h8 	&kp H
-			#define _0h9 	&kp S
-			#define _0h10	&kp N
+			#define _0h7 	&hmr RSHIFT T
+			#define _0h8 	&hmr RGUI H
+			#define _0h9 	&hmr RCTRL S
+			#define _0h10	&hmr RALT N
 			#define _0h11	&amt RC(BSPC) BSPC
 		// bottom row
 			#define _0b6 	&none
